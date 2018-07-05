efc45059362a33a4124de30c34e078574a01e697
franco.vegini <franco.vegini@senior.com.br>
2018-07-05 09:13:27 -0300

    Refatorado projeto para Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 import { Router } from '@angular/router';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
@@ -11,6 +13,8 @@ import { Order, OrderItem } from './order.model';
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup;
+
   // Valor do frete será fixo em 8 reais
   // Em uma aplicação real, o correto seria trazer esse valor do backend
   delivery: number = 8;
@@ -21,11 +25,21 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão de Débito', value: 'DEB' }
   ]
 
-  constructor(private orderService: OrderService, 
-              private router: Router) { }
+  constructor(private orderService: OrderService, //
+              private router: Router, 
+              private formBuilder: FormBuilder) { } 
 
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    })
   }
 
   itemsValue(): number {
@@ -52,11 +66,11 @@ export class OrderComponent implements OnInit {
     order.orderItems = this.cartItems()
       .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id));
     this.orderService.checkOrder(order)
-      .subscribe( (orderId: string) => {
+      .subscribe((orderId: string) => {
         this.router.navigate(['/order-summary'])
         this.orderService.clear();
 
-    })
+      })
     console.log(order);
   }
 
