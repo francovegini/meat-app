597bacf6a960e8700d0f4adfc3c2a760103293cb
Franco <francovegini@hotmail.com>
2018-07-08 14:03:49 -0300

    Removido Core Module e adicionado os serviços a um module compartilhado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { OrderService } from "../order/order.service";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
     imports: [FormsModule, ReactiveFormsModule, CommonModule],
     exports: [InputComponent, RadioComponent, RatingComponent, FormsModule, ReactiveFormsModule, CommonModule]
 })
-export class SharedModule{}
+export class SharedModule{
+    static forRoot(): ModuleWithProviders{
+        return {
+            ngModule: SharedModule,
+            providers: [OrderService, ShoppingCartService, RestaurantsService]
+        }
+    }
+}
