6102d9f29683e95993fb67898c443a26d6040e57
Franco <francovegini@hotmail.com>
2018-07-07 23:35:45 -0300

    Introduzindo Lazy Loading e criado module para o About

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
-import { AboutComponent } from "./about/about.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
@@ -20,7 +19,7 @@ export const ROUTES: Routes = [
         ]
     },
 
-    { path: 'about', component: AboutComponent },
+    { path: 'about', loadChildren: './about/about.module#AboutModule'},
     { path: 'order', component: OrderComponent },
     { path: 'order-summary', component: OrderSummaryComponent }
 ]
