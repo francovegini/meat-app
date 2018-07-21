2b6272b9c8ac2d9faf38ff07cdea01c3336fc62b
Franco <francovegini@hotmail.com>
2018-07-21 02:44:24 -0300

    Refactoring da aplicação para Angular 4.3

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,19 +1,17 @@
-import {Response} from '@angular/http'
-import {Observable} from 'rxjs/Observable'
+import { HttpErrorResponse } from '@angular/common/http'
+import { Observable } from 'rxjs/Observable'
 
 export class ErrorHandler {
-    static handleError(error: Response | any){
+    static handleError(error: HttpErrorResponse | any) {
         let errorMsg: string;
 
-        if(error instanceof Response){
-            errorMsg = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
+        if (error instanceof HttpErrorResponse) {
+            const body = error.error;
+            errorMsg = `${error.url}: ${error.status} - ${error.statusText || ''} ${body}`
         } else {
-            errorMsg = error.toString()
+            errorMsg = error.message ? error.message : error.toString();
         }
-
         console.log(errorMsg);
         return Observable.throw(errorMsg);
-        
-
     }
 }
