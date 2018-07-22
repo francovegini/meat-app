6b951d7677cf39d8134a09aacbc543d15c45a590
Franco <francovegini@hotmail.com>
2018-07-22 01:55:15 -0300

    Adicionado Token JWT a Rota de Login + Middleware de autorização

--- backend/users.ts
+++ backend/users.ts
@@ -7,10 +7,11 @@ export class User{
        return another !== undefined &&
         another.email === this.email &&
         another.password === this.password
-   }             
+   }   
+
 }
 
-export const users = {
+export const users: {[key: string]: User} = {
     "franco@gmail.com": new User('franco@gmail.com', 'Franco', '123'),
     "pedro@gmail.com": new User('pedro@gmail.com', 'Pedro', '123'),
     "amanda@gmail.com": new User('amanda@gmail.com', 'Amanda', '123')
