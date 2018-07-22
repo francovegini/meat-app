d60cea67c5bd95f0db4172f28109d0cf888cb2af
Franco <francovegini@hotmail.com>
2018-07-22 17:25:05 -0300

    Realizando compra com autenticação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
 import { Observable } from "rxjs/Observable";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map'
 import { Order, OrderItem } from './order.model';
 import { MEAT_API } from '../app.api'
+import { LoginService } from '../security/login/login.service'
+import { UrlSerializer } from "@angular/router";
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
+    constructor(private cartService: ShoppingCartService,
+                private http: HttpClient,
+                private loginService: LoginService) { }
 
     itemsValue(): number {
         return this.cartService.total();
@@ -37,7 +41,12 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        return this.http.post<Order>(`${MEAT_API}/orders`, order)
+        let headers = new HttpHeaders();
+        if(this.loginService.isLoggedIn()){
+            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
+        }
+
+        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers:headers})
                     .map(order => order.id)
     }
 
