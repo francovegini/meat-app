2cd0b008cedc07dbb0f45d15f3b549b567801d0f
franco.vegini <franco.vegini@senior.com.br>
2018-07-25 09:54:17 -0300

    Utilizado zonas para ApplicationErrorHandler

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { NotificationService } from './shared/messages/notification.service';
 import { LoginService } from './security/login/login.service';
 
@@ -8,7 +8,8 @@ import { LoginService } from './security/login/login.service';
 export class ApplicationErrorHandler extends ErrorHandler {
 
     constructor(private notificationService: NotificationService,
-                private injector: Injector) {
+                private injector: Injector,
+                private zone: NgZone) {
         super();
     }
 
@@ -17,18 +18,20 @@ export class ApplicationErrorHandler extends ErrorHandler {
             const message = errorResponse.error.message;
             const loginService = this.injector.get(LoginService);
 
-            switch (errorResponse.status) {
-                case 401:
-                    loginService.handleLogin();
-                    break;
-                case 403:
-                    this.notificationService.notify(message || "Não autorizado.");
-                    break;
-                case 404:
-                    this.notificationService.notify(message || "Recurso não encontrado.");
-                    break;
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        loginService.handleLogin();
+                        break;
+                    case 403:
+                        this.notificationService.notify(message || "Não autorizado.");
+                        break;
+                    case 404:
+                        this.notificationService.notify(message || "Recurso não encontrado.");
+                        break;
+                }
+            });
 
-            }
         }
 
         super.handleError(errorResponse);
