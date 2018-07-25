cbec8a5adb3e0e74500f45b1a81b713cf2642551
franco.vegini <franco.vegini@senior.com.br>
2018-07-25 09:18:05 -0300

    Utilizado HTTP Interceptors para conter Authorization em todas chamadas

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -6,15 +6,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map'
 import { Order, OrderItem } from './order.model';
 import { MEAT_API } from '../app.api'
-import { LoginService } from '../security/login/login.service'
 import { UrlSerializer } from "@angular/router";
 
 @Injectable()
 export class OrderService {
 
     constructor(private cartService: ShoppingCartService,
-                private http: HttpClient,
-                private loginService: LoginService) { }
+        private http: HttpClient) { }
 
     itemsValue(): number {
         return this.cartService.total();
@@ -36,18 +34,13 @@ export class OrderService {
         this.cartService.removeItem(item);
     }
 
-    clear(){
+    clear() {
         this.cartService.clear();
     }
 
     checkOrder(order: Order): Observable<string> {
-        let headers = new HttpHeaders();
-        if(this.loginService.isLoggedIn()){
-            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
-        }
-
-        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers:headers})
-                    .map(order => order.id)
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
+            .map(order => order.id)
     }
 
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,6 +12,8 @@ import { NotificationService } from "./messages/notification.service";
 import { LoginService } from "../security/login/login.service";
 import { LoggedInGuard } from "../security/loggedin.guard";
 import { LeaveOrderGuard } from '../order/leave-order.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import { AuthInterceptor } from "../security/auth.interceptor";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -28,7 +30,8 @@ export class SharedModule {
                 NotificationService,
                 LoginService,
                 LoggedInGuard,
-                LeaveOrderGuard]
+                LeaveOrderGuard,
+                {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
         }
     }
 }
