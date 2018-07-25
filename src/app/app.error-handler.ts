dcc020ab782893e949d283e0c50d0d25f5c0bf29
franco.vegini <franco.vegini@senior.com.br>
2018-07-25 09:43:09 -0300

    Criado Error Handler Global

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,17 +1,36 @@
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { NotificationService } from './shared/messages/notification.service';
+import { LoginService } from './security/login/login.service';
 
-export class ErrorHandler {
-    static handleError(error: HttpErrorResponse | any) {
-        let errorMsg: string;
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
 
-        if (error instanceof HttpErrorResponse) {
-            const body = error.error;
-            errorMsg = `${error.url}: ${error.status} - ${error.statusText || ''} ${body}`
-        } else {
-            errorMsg = error.message ? error.message : error.toString();
+    constructor(private notificationService: NotificationService,
+                private injector: Injector) {
+        super();
+    }
+
+    handleError(errorResponse: HttpErrorResponse | any) {
+        if (errorResponse instanceof HttpErrorResponse) {
+            const message = errorResponse.error.message;
+            const loginService = this.injector.get(LoginService);
+
+            switch (errorResponse.status) {
+                case 401:
+                    loginService.handleLogin();
+                    break;
+                case 403:
+                    this.notificationService.notify(message || "Não autorizado.");
+                    break;
+                case 404:
+                    this.notificationService.notify(message || "Recurso não encontrado.");
+                    break;
+
+            }
         }
-        console.log(errorMsg);
-        return Observable.throw(errorMsg);
+
+        super.handleError(errorResponse);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
-
 import { ROUTES } from './app.routes';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -19,9 +17,9 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component'
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
+import { ApplicationErrorHandler } from './app.error-handler';
 
 
 @NgModule({
@@ -48,7 +46,8 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' },
+              { provide: ErrorHandler, useClass: ApplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
