92dc6c3cae6cfd118e29911e051384c90468e9cb
franco.vegini <franco.vegini@senior.com.br>
2018-07-26 09:24:40 -0300

    Atualização das dependências para Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,10 @@ import { NotFoundComponent } from './not-found/not-found.component'
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
 import { ApplicationErrorHandler } from './app.error-handler';
+import {registerLocaleData} from '@angular/common'
+import localePt from '@angular/common/locales/pt'
 
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -46,7 +49,7 @@ import { ApplicationErrorHandler } from './app.error-handler';
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' },
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' },
               { provide: ErrorHandler, useClass: ApplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
