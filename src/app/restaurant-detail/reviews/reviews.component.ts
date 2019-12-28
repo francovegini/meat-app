05646cc4d2d01cf6372dc5601f7a3c62fd5a095d
Franco Vegini <francovegini@hotmail.com>
2019-12-28 16:55:58 -0200

    ajustado reviews.component

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RestaurantsService } from '../../restaurants/restaurants.service';
-
 import { Observable } from 'rxjs';
 
 @Component({
